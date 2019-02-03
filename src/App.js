f9d922a1eb975d1bd94929892df33479a6825e15
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-02-04 01:10:24 +0200

    Add stage 1 of mapping: script loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+//first part to add a map to the App, loading the API
+// adding scripts into react https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
+
+componentDidMount() {
+  const script = document.createElement("script");
+
+  script.src = "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCaOk9oP1NSXBtSPcrgHAO2fL3gu_UQzWs&v=3";
+  script.async = true;
+  script.defer = true;
+
+  document.body.appendChild(script);
+  console.log(script);
+}
+
+
   render() {
     return (
       <div className="map">

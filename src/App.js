9a9eefd694c9b7b38111e4e7e7647d8b6b7bbe0b
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-02-04 23:41:20 +0200

    Added requested parts to load the map

--- src/App.js
+++ src/App.js
@@ -3,27 +3,46 @@ import './App.css';
 
 class App extends Component {
 
-//first part to add a map to the App, loading the API
-// adding scripts into react https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
-
 componentDidMount() {
-  const script = document.createElement("script");
-
-  script.src = "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCaOk9oP1NSXBtSPcrgHAO2fL3gu_UQzWs&v=3";
-  script.async = true;
-  script.defer = true;
-
-  document.body.appendChild(script);
-  console.log(script);
+  this.loadMap()
+  //console.log(this.loadMap)
 }
 
+loadMap = () => {
+  window.initMap = this.initMap
+  //console.log(window.initMap)
+}
+
+
+//third required part to load the map, map variable plus js function to load the map
+//https://developers.google.com/maps/documentation/javascript/tutorial
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 44.439663, lng: 26.096306},
+      zoom: 14
+    });
+  }
 
   render() {
     return (
-      <div className="map">
-      </div>
+      <main>
+      {/*second request to add the map, a div with the id of map*/}
+        <div id="map">
+        </div>
+      </main>
     );
   }
 }
+
+//first part to add the map to the App, loading the API
+// adding scripts into react https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
+
+const loadScript = document.createElement("script");
+
+loadScript.src = "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCaOk9oP1NSXBtSPcrgHAO2fL3gu_UQzWs&v=3&callback=initMap";
+loadScript.async = true;
+loadScript.defer = true;
+document.body.appendChild(loadScript);
+//console.log(loadScript);
 
 export default App;

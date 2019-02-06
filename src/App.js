8bf54414463cc7732b8f05417fe53ec28352260c
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-02-06 17:01:57 +0200

    Added async function to load foursquare api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
-componentDidMount() {
-  this.loadMap()
-  //console.log(this.loadMap)
+//state of component which will receive info from foursquare api
+state = {
+  venues: [] //empty array which will be filled once the function getinfo loads the information from api async request
 }
 
-loadMap = () => {
-  window.initMap = this.initMap
-  //console.log(window.initMap)
-}
+  componentDidMount() {
+    //when the componentDidMount starts the loadmap function should be loaded and should call getinfo function to load foursquare api
+    this.loadMap();
+    this.getInfo();
+    //console.log(this.getInfo);
+    //console.log(this.loadMap)
+  }
 
+  loadMap = () => {
+    window.initMap = this.initMap;
+    //console.log(window.initMap)
+  };
+  //https://developer.foursquare.com/docs/api
+  //create venue app using react with foursquare api http://stevebrown.co/journal/creating-a-local-venue-app-using-reactredux-with-the-foursquare-api-part-i
+  // react docs, ajax and apis https://reactjs.org/docs/faq-ajax.html
+  getInfo() {
 
-//third required part to load the map, map variable plus js function to load the map
-//https://developers.google.com/maps/documentation/javascript/tutorial
+    const venuesAPI = "https://api.foursquare.com/v2/venues/explore?";
+    const parameters = {
+      client_id: "RI1LDPFFA2C2K0C0DP0TBBY2JTWA30F2O0DNMJTZRQGVXL3P",
+      client_secret: "P2RAP3W3EO4MGJDOVUQEA0FG1YEWMG23LMR00A3WHZ55GNQL",
+      query: "drinks",
+      near: "bucarest",
+      v: "20190602",
+    }
+
+    fetch(venuesAPI + new URLSearchParams(parameters), {
+      method: "GET"
+    }).then(response =>
+      response.json()).then(response => {
+        this.setState({venues: response.response.groups[0].items})
+      })
+      //console.log(response);
+      .catch(error => {
+        // Code for handling errors
+        console.log("error " + error);
+      });
+    }
+
+  //third required part to load the map, map variable plus js function to load the map
+  //https://developers.google.com/maps/documentation/javascript/tutorial
   initMap = () => {
-    var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 44.439663, lng: 26.096306},
+    var map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 44.439663, lng: 26.096306 },
       zoom: 14
     });
-  }
+  };
+
+//marker function should load at the same time with google maps function, they should display simultaneosly
 
   render() {
     return (
       <main>
-      {/*second request to add the map, a div with the id of map*/}
-        <div id="map">
-        </div>
+        {/*second request to add the map, a div with the id of map*/}
+        <div id="map" />
       </main>
     );
   }
@@ -39,7 +72,8 @@ loadMap = () => {
 
 const loadScript = document.createElement("script");
 
-loadScript.src = "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCaOk9oP1NSXBtSPcrgHAO2fL3gu_UQzWs&v=3&callback=initMap";
+loadScript.src =
+  "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyCaOk9oP1NSXBtSPcrgHAO2fL3gu_UQzWs&v=3&callback=initMap";
 loadScript.async = true;
 loadScript.defer = true;
 document.body.appendChild(loadScript);

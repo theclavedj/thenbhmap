25c84b6953041d1a4f67af3702521d9cd1175476
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-03-30 13:39:45 +0200

    Disabled SW

---
+++ src/sw.js
@@ -0,0 +1,7 @@
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3/"});
+
+const precacheManifest = [];
+
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
